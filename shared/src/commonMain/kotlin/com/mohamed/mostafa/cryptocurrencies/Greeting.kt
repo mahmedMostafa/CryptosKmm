05694ebd4b28450557b48fe8e1ab8499b2452eb4
Alien <alien@Aliens-MacBook-Pro.local>
2021-11-13 01:05:34 +0200

    deleted unsed calsses

--- shared/src/commonMain/kotlin/com/mohamed/mostafa/cryptocurrencies/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.mohamed.mostafa.cryptocurrencies
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/mohamed/mostafa/cryptocurrencies/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.mohamed.mostafa.cryptocurrencies
-
-expect class Platform() {
-    val platform: String
-}
