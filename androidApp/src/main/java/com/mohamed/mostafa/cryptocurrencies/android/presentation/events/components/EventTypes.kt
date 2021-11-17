c9fb9dae88de3c109e303d90a7878ead91bae33a
mahmedMostafa <alienghost12@gmail.com>
2021-11-17 14:37:02 +0200

    added tab color transition

--- androidApp/src/main/java/com/mohamed/mostafa/cryptocurrencies/android/presentation/events/components/EventTypes.kt
+++ androidApp/src/main/java/com/mohamed/mostafa/cryptocurrencies/android/presentation/events/components/EventTypes.kt
@@ -3,6 +3,7 @@ package com.mohamed.mostafa.cryptocurrencies.android.presentation.events.compone
 
 import android.graphics.Color.TRANSPARENT
 import android.graphics.Color.WHITE
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -11,14 +12,19 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun EventTypes(types: List<String>, selectedType: String, onItemClicked: (String) -> Unit) {
+
     LazyRow(
 //        modifier = Modifier.padding(18.dp)
     ) {
@@ -35,6 +41,11 @@ fun EventTypes(types: List<String>, selectedType: String, onItemClicked: (String
 
 @Composable
 fun EventTypeItem(title: String, isSelected: Boolean = false, onItemClicked: (String) -> Unit) {
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isSelected) MaterialTheme.colors.primary else Color(
+            WHITE
+        )
+    )
     Text(
         title,
         style = MaterialTheme.typography.body1.copy(
@@ -51,7 +62,7 @@ fun EventTypeItem(title: String, isSelected: Boolean = false, onItemClicked: (St
                 shape = RoundedCornerShape(8.dp),
             )
             .background(
-                if (isSelected) MaterialTheme.colors.primary else Color(WHITE),
+                backgroundColor,
                 shape = RoundedCornerShape(8.dp)
             )
             .padding(horizontal = 10.dp, vertical = 6.dp),
